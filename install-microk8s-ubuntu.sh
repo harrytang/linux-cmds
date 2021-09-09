cb3b301ecd60eb0fb4e3f958e18d4835696d9a22
Harry Tang <harry@powerkernel.com>
2021-09-09 21:19:16 +0300

    Update install-microk8s-ubuntu.sh

--- install-microk8s-ubuntu.sh
+++ install-microk8s-ubuntu.sh
@@ -2,3 +2,5 @@
 sudo apt update && sudo apt upgrade -y
 sudo sed -i "s/\/\/Unattended-Upgrade::Automatic-Reboot \"false\";/Unattended-Upgrade::Automatic-Reboot \"true\";/g" /etc/apt/apt.conf.d/50unattended-upgrades
 sudo snap install microk8s --classic --channel=1.22/stable
+sudo usermod -a -G microk8s ubuntu
+sudo chown -f -R ubuntu ~/.kube
