7c1b0ba41cf737f76d726e34be1623ea29fa799a
Harry Tang <harry@powerkernel.com>
2021-09-10 15:37:30 +0300

    Update install-docker-oracle-linux-8.sh

--- install-docker-oracle-linux-8.sh
+++ install-docker-oracle-linux-8.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+sudo yum update -y
 sudo dnf install dnf-automatic -y
 sudo sed -i "s/apply_updates = no/apply_updates = yes/g" /etc/dnf/automatic.conf
 sudo systemctl enable --now dnf-automatic.timer
-sudo yum update -y
 sudo dnf install -y dnf-utils zip unzip
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install -y docker-ce --nobest
