caf0862f126d165c504578c2d59956768605ddc0
Harry Tang <harry@powerkernel.com>
2021-09-12 13:21:52 +0300

    Update install-docker-ubuntu-amd64.sh

--- install-docker-ubuntu-amd64.sh
+++ install-docker-ubuntu-amd64.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo apt update && sudo apt upgrade -y
+sudo sed -i "s/\/\/Unattended-Upgrade::Automatic-Reboot \"false\";/Unattended-Upgrade::Automatic-Reboot \"true\";/g" /etc/apt/apt.conf.d/50unattended-upgrades
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
